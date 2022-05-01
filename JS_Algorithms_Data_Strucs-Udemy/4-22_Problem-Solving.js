f7fd8ea701b533a0bd7aab6f4fbd687908219aad
Nick Edwards <theadventuresofnickedwards@gmail.com>
2022-05-01 16:46:21 -0700

    Testing char coutners

---
+++ JS_Algorithms_Data_Strucs-Udemy/4-22_Problem-Solving.js
@@ -0,0 +1,52 @@
+// SOLVE/SIMPLIFY
+// Write a function which takes a string and returns counts of each character in the string
+
+function characterCount(string) {
+    // make an object to return at end
+    let finalTallies = {};
+    // loop over string
+    for (let i = 0; i < string.length; i++) {
+        // if char is NOT alphanumeric, don't do anything???
+        // if char is alphanumeric and key in object
+        let character = string[i].toLowerCase();
+        if (finalTallies[character] > 0) {
+            finalTallies[character]++;
+        // if char is alhanumeric and NOT key in object
+        } else {
+            // add char to object and set value to 1
+            finalTallies[character] = 1;
+        }
+    }
+    // return object
+    return finalTallies;
+}
+
+// Why doesn't this work??? 😭
+
+// REVISED
+function charCount(str) {
+    var obj = {};
+    for (let i = 0; i < str.length; i++) {
+        var char = str[i].toLowerCase();
+        if (/[a-z0-9]/.test(char)) {
+            if (obj[char] > 0) {
+                obj[char]++;
+            } else {
+                obj[char] = 1;1
+            };
+        }
+    }
+    return obj;
+}
+
+// REFACOTRED
+function charCounter(str) {
+    var obj = {};
+    for (var char of str) {
+        char = char.toLowerCase();
+        if (/[a-z0-9]/.test(char)) {
+            obj[char] = ++obj[char] || 1;
+        }
+    }
+    return obj;
+}

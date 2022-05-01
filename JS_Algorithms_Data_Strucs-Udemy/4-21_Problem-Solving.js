f830f1b3803be487681a093570442ef35b4235b2
Nick Edwards <theadventuresofnickedwards@gmail.com>
2022-05-01 15:47:34 -0700

    Problem solving, break it down

--- JS_Algorithms_Data_Strucs-Udemy/4-21_Problem-Solving.js
+++ JS_Algorithms_Data_Strucs-Udemy/4-21_Problem-Solving.js
@@ -1,1 +1,12 @@
 // BREAK IT DOWN
+// Write a function which takes a string and returns counts of each character in the string
+
+function charCount(str) {
+    // make an object to return at end
+    // loop over string
+        // if char is alphanumeric and key in object
+        // if char is alhanumeric and NOT key in object
+            // add char to object and set value to 1
+        // if char is NOT alphanumeric, don't do anything
+    // return object
+}
